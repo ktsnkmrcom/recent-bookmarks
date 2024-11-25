43ed8b4bdcef5e7e4ba6dfcaa87ae9665169ffeb
ktsnkmrcom <ktsnkmrcom@gmail.com>
2024-11-26 01:32:54 +0900

    first commit

---
+++ popup.js
@@ -0,0 +1,26 @@
+function bookmark() {
+  chrome.bookmarks.getRecent(10, function (bookmarkTreeNodes) {
+    const frag = document.createDocumentFragment();
+
+    for (let bookmarkNodes of bookmarkTreeNodes) {
+      const li = document.createElement("li");
+      const anchor = document.createElement("a");
+
+      anchor.href = bookmarkNodes.url;
+      anchor.text = bookmarkNodes.title;
+
+      li.append(anchor);
+      frag.append(li);
+
+      anchor.addEventListener("click", function () {
+        chrome.tabs.create({ url: bookmarkNodes.url });
+      });
+    }
+
+    document.querySelector("#recent").append(frag);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  bookmark();
+});
